f161eb15d6bacb0baf9f24f8013171d989ff4e9b
Mrinal kanti sardar <64627885+Mrinmax@users.noreply.github.com>
2021-12-21 00:16:47 +0530

    input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <h1> React Todo App</h1>
+      <Input />
     </div>
   );
 }
